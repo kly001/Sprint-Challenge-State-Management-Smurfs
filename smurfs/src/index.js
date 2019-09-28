328d78cd0b0e393d1a74f0d4cb7d203483048485
kly001 <karenly1961@gmail.com>
2019-09-28 12:23:28 -0400

    Edited src/index

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import smurfReducer from "./reducers"
+
+import "./index.css";
+
+
+const store = createStore(smurfReducer,
+    applyMiddleware(thunk))
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
+
